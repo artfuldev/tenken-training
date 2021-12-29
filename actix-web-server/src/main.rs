00502ac6f6a0b95b2b50eaaec86ba62218d59b2b
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2021-12-29 11:35:28 +0000

    Initialize database values into memory

--- actix-web-server/src/main.rs
+++ actix-web-server/src/main.rs
@@ -1,9 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use chashmap::CHashMap;
-use std::io::*;
-use std::fs::OpenOptions;
+use std::io::{self, *};
+use std::fs::{OpenOptions, File};
 use std::ops::Deref;
 use web::*;
+use serde_json::{to_string, from_str};
 
 mod message;
 use crate::message::Message;
@@ -16,7 +17,7 @@ async fn hello() -> impl Responder {
 #[post("/probe/{probe_id}/event/{event_id}")]
 async fn store_message(
     Path((probe_id, _)): Path<(String, String)>,
-    message: Json<Message>,
+    json: Json<Message>,
     app_data: Data<CHashMap<String, Message>>,
 ) -> impl Responder {
     let mut file = OpenOptions::new()
@@ -24,9 +25,10 @@ async fn store_message(
         .append(true)
         .open("db.dat")
         .unwrap();
-    match writeln!(file, "{}, {:?}", probe_id, message) {
+    let message = json.into_inner();
+    match writeln!(file, "{}:::{}", probe_id, to_string(&message).unwrap()) {
         Ok(_) =>  {
-            app_data.insert(probe_id, message.into_inner());
+            app_data.insert(probe_id, message);
             HttpResponse::Ok().body("Created")
         },
         Err(e) => {
@@ -46,9 +48,28 @@ async fn get_message(
     HttpResponse::Ok().body(data.map(|x| x.deref().eventId.clone()).unwrap_or_default())
 }
 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<std::path::Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let initial_state: Data<CHashMap<String, Message>> = Data::new(CHashMap::new());
+    let state = CHashMap::new();
+    if let Ok(lines) = read_lines("db.dat") {
+        for line in lines {
+            if let Ok(entry) = line {
+                let splits = entry.split(":::").collect::<Vec<&str>>();
+                let key = splits[0];
+                let value = splits[1];
+                println!("reading probe id {} message {}", key, value);
+                let message: Message = from_str(value).unwrap();
+                state.insert(key.to_string(), message);
+            }
+        }
+    }
+    let initial_state = Data::new(state);
     HttpServer::new(move || {
         let eight_bytes = 8192;
         let json_config = JsonConfig::default().limit(eight_bytes);
