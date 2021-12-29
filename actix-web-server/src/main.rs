acea21b0859c5fad0a0f45aa9ba1fb4c0348af85
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2021-12-29 11:08:30 +0000

    Use a shared in-memory cache

--- actix-web-server/src/main.rs
+++ actix-web-server/src/main.rs
@@ -1,13 +1,15 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use chashmap::CHashMap;
+use std::io::*;
 use std::fs::OpenOptions;
-use std::io::Write;
 use std::ops::Deref;
 
 mod message;
 use crate::message::Message;
 
+#[derive(Clone)]
 struct AppState {
-    in_memory_store: chashmap::CHashMap<String, Message>,
+    in_memory_store: CHashMap<String, Message>,
 }
 
 #[get("/")]
@@ -17,19 +19,26 @@ async fn hello() -> impl Responder {
 
 #[post("/probe/{probe_id}/event/{event_id}")]
 async fn store_message(
-    web::Path((probe_id, event_id)): web::Path<(String, String)>,
+    web::Path((probe_id, _)): web::Path<(String, String)>,
     message: web::Json<Message>,
     app_data: web::Data<AppState>,
 ) -> impl Responder {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("my-file")
+        .open("db.dat")
         .unwrap();
-    if let Err(e) = writeln!(file, "{}, {:?}", probe_id, message) {
-        eprintln!("Couldn't write to file: {}", e);
+    match writeln!(file, "{}, {:?}", probe_id, message) {
+        Ok(_) =>  {
+            app_data.in_memory_store.insert(probe_id, message.into_inner());
+            HttpResponse::Ok().body("Created")
+        },
+        Err(e) => {
+            let body = format!("Couldn't write to file: {}", e);
+            eprintln!("{}", body);
+            HttpResponse::InternalServerError().body(body)
+        }
     }
-    HttpResponse::Ok().body("Created")
 }
 
 #[get("/probe/{probe_id}/latest")]
@@ -37,22 +46,20 @@ async fn get_message(
     web::Path(probe_id): web::Path<String>,
     app_data: web::Data<AppState>,
 ) -> impl Responder {
-    let data = app_data.get_ref().in_memory_store.get(&probe_id);
-    HttpResponse::Ok().body(data.map(|x| x.deref().probeId.clone()).unwrap_or_default())
+    let data = app_data.in_memory_store.get(&probe_id);
+    HttpResponse::Ok().body(data.map(|x| x.deref().eventId.clone()).unwrap_or_default())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let initial_state = web::Data::new(AppState { in_memory_store: CHashMap::new() });
+    HttpServer::new(move || {
         let eight_bytes = 8192;
         let json_config = web::JsonConfig::default().limit(eight_bytes);
-        let init_data = web::Data::new(AppState {
-            in_memory_store: chashmap::CHashMap::new(),
-        });
 
         App::new()
             .app_data(json_config)
-            .app_data(init_data)
+            .app_data(initial_state.clone())
             .service(hello)
             .service(store_message)
             .service(get_message)
