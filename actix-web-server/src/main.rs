ef30d813dc20cc7315c30dbdccee3aeb16322013
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-01-02 19:46:24 +0000

    Use an in-memory data store

--- actix-web-server/src/main.rs
+++ actix-web-server/src/main.rs
@@ -1,16 +1,12 @@
-use actix::{Addr, Actor};
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use chashmap::CHashMap;
-use std::io::{self, *};
-use std::fs::{OpenOptions, File};
+use std::collections::HashMap;
 use std::ops::Deref;
+use std::sync::Mutex;
 use web::*;
-use serde_json::{to_string, from_str};
-
+use serde_json::{to_string};
 mod dto;
 mod writer;
 use crate::dto::{ProbeData, ProbeRequest, ProbeResponse};
-use crate::writer::{Writer, ProbePayloadReceived};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -21,30 +17,21 @@ async fn hello() -> impl Responder {
 async fn store_message(
     Path((probe_id, _)): Path<(String, String)>,
     json: Json<ProbeRequest>,
-    cache: Data<CHashMap<String, ProbeData>>,
-    writer: Data<Addr<Writer>>
+    cache: Data<Mutex<HashMap<String, ProbeData>>>,
 ) -> impl Responder {
     let data = ProbeData::from(json.into_inner());
-    let result = writer.send(ProbePayloadReceived::new(probe_id.clone(), data.clone())).await.unwrap();
-    match result {
-        Ok(_) =>  {
-            cache.insert(probe_id, data);
-            HttpResponse::Accepted().body("")
-        },
-        Err(e) => {
-            let body = format!("Couldn't write to file: {:?}", e);
-            eprintln!("{}", body);
-            HttpResponse::InternalServerError().body(body)
-        }
-    }
+    cache.lock().unwrap().insert(probe_id, data);
+    HttpResponse::Accepted().body("")
 }
 
 #[get("/probe/{probe_id}/latest")]
 async fn get_message(
     Path(probe_id): Path<String>,
-    cache: Data<CHashMap<String, ProbeData>>,
+    cache: Data<Mutex<HashMap<String, ProbeData>>>,
 ) -> impl Responder {
       cache
+        .lock()
+        .unwrap()
         .get(&probe_id)
         .map(|x| x.deref().clone())
         .map(ProbeResponse::from)
@@ -55,42 +42,18 @@ async fn get_message(
         .unwrap_or(HttpResponse::NotFound().body(""))
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<std::path::Path>, {
-    let file = OpenOptions::new().read(true).create(true).open(filename.as_ref())?;
-    Ok(io::BufReader::new(file).lines())
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let _cache = CHashMap::new();
-    if let Ok(lines) = read_lines("db.dat") {
-        for line in lines {
-            if let Ok(entry) = line {
-                let splits = entry.split(":::").collect::<Vec<&str>>();
-                let key = splits[0];
-                let value = splits[1];
-                println!("reading probe id {} data {}", key, value);
-                let data: ProbeData = from_str(value).unwrap();
-                _cache.insert(key.to_string(), data);
-            }
-        }
-    }
-    let writer = Data::new(Writer::new()?.start());
-    let cache = Data::new(_cache);
+    let cache = Data::new(Mutex::new(HashMap::<String, ProbeData>::new()));
     HttpServer::new(move || {
-        let eight_bytes = 8192;
-        let json_config = JsonConfig::default().limit(eight_bytes);
-
         App::new()
-            .app_data(json_config)
             .app_data(cache.clone())
-            .app_data(writer.clone())
             .service(hello)
             .service(store_message)
             .service(get_message)
     })
     .bind("127.0.0.1:8080")?
+    .workers(8)
     .run()
     .await
 }
