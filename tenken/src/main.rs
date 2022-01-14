984b812c7d24386aab5466e4ef374a95395ccb40
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-01-14 22:37:33 +0000

    Use tenken actor

--- tenken/src/main.rs
+++ tenken/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::RwLock;
+use actix::{Actor, Addr};
 use actix_web::{dev::Body, get, post, web, App, HttpResponse, HttpServer, Responder};
 use web::*;
 
@@ -13,28 +13,34 @@ async fn hello() -> impl Responder {
 #[post("/probe/{probe_id}/event/{event_id}")]
 async fn store_message(
     Path((probe_id, _)): Path<(String, String)>,
-    text: String,
-    db: Data<RwLock<Tenken>>,
+    payload: String,
+    db: Data<Addr<Tenken>>,
 ) -> impl Responder {
-    db.write().unwrap()
-        .put(probe_id, text);
+    db.do_send(ProbePayloadReceived::new(probe_id, payload));
     HttpResponse::Accepted().body(Body::Empty)
 }
 
 #[get("/probe/{probe_id}/latest")]
 async fn get_message(
     Path(probe_id): Path<String>,
-    db: Data<RwLock<Tenken>>,
+    db: Data<Addr<Tenken>>,
 ) -> impl Responder {
-    db.read().unwrap()
-        .get(probe_id)
-        .map(|x| HttpResponse::Ok().body(x))
-        .unwrap_or(HttpResponse::NotFound().body(Body::Empty))
+    match db.send(ProbeRequestReceived::new(probe_id)).await {
+        Ok(response) =>
+            match response {
+                Ok(result) =>
+                    result
+                        .map(|x| HttpResponse::Ok().content_type("application/json").body(x))
+                        .unwrap_or(HttpResponse::NotFound().body(Body::Empty)),
+                Err(_) => HttpResponse::InternalServerError().body(Body::Empty)
+            },
+        Err(_) => HttpResponse::InternalServerError().body(Body::Empty)
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = Data::new(RwLock::new(Tenken::default()));
+    let db = Data::new(Tenken::default().start());
     HttpServer::new(move || {
         App::new()
             .app_data(db.clone())
