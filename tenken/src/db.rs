e43178cff5643fe00332175480a830275c145162
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-01-14 22:09:58 +0000

    Make tenken an actor

--- tenken/src/db.rs
+++ tenken/src/db.rs
@@ -1,4 +1,6 @@
 use fxhash::FxHashMap;
+use actix::{Actor, Message, Handler};
+use thiserror::Error;
 
 pub trait Database<K, V> {
     fn get(&self, key: K) -> Option<V>;
@@ -13,6 +15,40 @@ impl Default for Tenken {
     fn default() -> Self {
         Tenken { cache: FxHashMap::<String, String>::default() }
     }
+}
+
+impl Actor for Tenken {
+    type Context = actix::Context<Self>;
+}
+
+pub struct ProbePayloadReceived {
+    probe_id: String,
+    payload: String
+}
+
+impl ProbePayloadReceived {
+    pub fn new(probe_id: String, payload: String) -> Self {
+      ProbePayloadReceived { probe_id, payload }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum TenkenError {
+  #[error("something failed")]
+  GenericError
+}
+
+impl Message for ProbePayloadReceived {
+    type Result = Result<(), TenkenError>;
+}
+
+impl Handler<ProbePayloadReceived> for Tenken {
+    type Result = Result<(), TenkenError>;
+
+    fn handle(&mut self, msg: ProbePayloadReceived, _ctx: &mut Self::Context) -> Self::Result {
+        self.put(msg.probe_id, msg.payload);
+        Ok(())
+    }   
 }
 
 impl Database<String, String> for Tenken {
