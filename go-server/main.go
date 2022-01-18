cd540d20dec8309035c926d3c01066441e0f8c0c
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-01-18 15:13:09 +0000

    Unify output

--- go-server/main.go
+++ go-server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
 
-    log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3000"))
 }
