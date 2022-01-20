ed3947d74d315df334b66ef725d4315e4a64bd1c
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-01-20 18:23:26 +0000

    Make structs debug friendly

--- actix-server/src/actors/transactor/entry.rs
+++ actix-server/src/actors/transactor/entry.rs
@@ -1,14 +1,17 @@
+#[derive(Debug)]
 pub struct EntryPreface<K> {
     pub key: K,
     pub timestamp: u64,
 }
 
+#[derive(Debug)]
 pub struct Entry<K, V> {
     pub key: K,
     pub timestamp: u64,
     pub value: V
 }
 
+#[derive(Debug)]
 pub struct EntryUpdate<V> {
     pub timestamp: u64,
     pub value: V
--- actix-server/src/actors/transactor/file_handle.rs
+++ actix-server/src/actors/transactor/file_handle.rs
@@ -1,15 +1,30 @@
+use std::error;
+
+use thiserror::Error;
+
 use super::entry::*;
 
+#[derive(Debug, Error)]
 pub enum FileHandleError {
+    #[error("failed to read into buffer")]
     ReadIntoBufferFailed,
+    #[error("failed to read data length")]
     ReadDataLengthFailed,
+    #[error("failed to convert data length offset")]
     DataLengthOffsetConversionFailed,
+    #[error("failed to read as string")]
     ReadDataAsStringFailed,
+    #[error("failed to read into header buffer")]
     ReadIntoHeaderBufferFailed,
+    #[error("failed to convert key size")]
     KeySizeConversionFailed,
+    #[error("failed to read key")]
     KeyReadFailed,
+    #[error("failed to read timestamp")]
     TimestampReadFailed,
+    #[error("failed to write key")]
     KeyWriteFailed,
+    #[error("failed to write update")]
     UpdateWriteFailed
 }
 
